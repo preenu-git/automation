1456805022ade0891ae19115b769f2fe56a400f5
Preenu Clarence <clarencepreenu@gmail.com>
2025-10-09 12:01:53 +0530

    push

--- AutomationCourse1/src/test/java/seleniumBasics/BrowserAndNavigationCommands.java
+++ AutomationCourse1/src/test/java/seleniumBasics/BrowserAndNavigationCommands.java
@@ -6,15 +6,28 @@ public class BrowserAndNavigationCommands extends Base{
 	{
 	String title =driver.getTitle();//Retrieves the title of the current web page
 	System.out.println(title);//prints the title in the console
-	String source=driver.getPageSource();
-	System.out.println(source);
+	//String source=driver.getPageSource();
+	//System.out.println(source);
+	String url=driver.getCurrentUrl();
+	System.out.println(url);
+	String handleId=driver.getWindowHandle();
+	System.out.println(handleId);
+	}
+	public void navigationCommand()
+	{
+		driver.navigate().to("https://www.amazon.in/");//opens obsqura testing to amazon
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().refresh();
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BrowserAndNavigationCommands command=new BrowserAndNavigationCommands();
 		command.initializeBrowser();
-		command.browerCommands();
+		//command.browerCommands();
+		command.navigationCommand();
+		
 		
 
 	}
